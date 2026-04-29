4eab568768128fab4baa74c76656760e7d9bded7
Jordan Sauro <jsauro@unikingcanada.com>
2026-04-29 13:35:43 +0000

    Update app pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/Home';
-import DonghuaChain from './pages/DonghuaChain';
-import Catalog from './pages/Catalog';
-import SpecialChains from './pages/SpecialChains';
-import ElevatorBuckets from './pages/ElevatorBuckets';
-import WeldedSteel from './pages/WeldedSteel';
-import Status from './pages/Status';
 
 function App() {
   return (
@@ -13,12 +7,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/Home" replace />} />
         <Route path="/Home" element={<Home />} />
-        <Route path="/DonghuaChain" element={<DonghuaChain />} />
-        <Route path="/Catalog" element={<Catalog />} />
-        <Route path="/SpecialChains" element={<SpecialChains />} />
-        <Route path="/ElevatorBuckets" element={<ElevatorBuckets />} />
-        <Route path="/WeldedSteel" element={<WeldedSteel />} />
-        <Route path="/Status" element={<Status />} />
       </Routes>
     </BrowserRouter>
   )
