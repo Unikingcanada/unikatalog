b755a5963f10e8054aafcbef4b5fc270c5f83838
Jordan Sauro <jsauro@unikingcanada.com>
2026-05-01 01:41:26 +0000

    Update app pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import RollerConfigurator from './pages/RollerConfigurator';
-import RFQCart from './pages/RFQCart';
-import Catalog from './pages/Catalog';
 import Home from './pages/Home';
+import Catalog from './pages/Catalog';
+import RFQCart from './pages/RFQCart';
 
 function App() {
   return (
@@ -10,9 +10,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/Home" replace />} />
         <Route path="/RollerConfigurator" element={<RollerConfigurator />} />
-        <Route path="/RFQCart" element={<RFQCart />} />
-        <Route path="/Catalog" element={<Catalog />} />
         <Route path="/Home" element={<Home />} />
+        <Route path="/Catalog" element={<Catalog />} />
+        <Route path="/RFQCart" element={<RFQCart />} />
       </Routes>
     </BrowserRouter>
   )
