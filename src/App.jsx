99bd67e49fb4d2310c227ef80bff8a368088221c
Jordan Sauro <jsauro@unikingcanada.com>
2026-05-03 09:17:46 +0000

    Update app pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import ForgedChain from './pages/ForgedChain';
-import Home from './pages/Home';
-import RFQCart from './pages/RFQCart';
 import ElevatorBuckets from './pages/ElevatorBuckets';
 import RollerConfigurator from './pages/RollerConfigurator';
 import ForgedChainConfigurator from './pages/ForgedChainConfigurator';
+import ForgedChain from './pages/ForgedChain';
 import Catalog from './pages/Catalog';
+import RFQCart from './pages/RFQCart';
+import Home from './pages/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/Home" replace />} />
-        <Route path="/ForgedChain" element={<ForgedChain />} />
-        <Route path="/Home" element={<Home />} />
-        <Route path="/RFQCart" element={<RFQCart />} />
         <Route path="/ElevatorBuckets" element={<ElevatorBuckets />} />
         <Route path="/RollerConfigurator" element={<RollerConfigurator />} />
         <Route path="/ForgedChainConfigurator" element={<ForgedChainConfigurator />} />
+        <Route path="/ForgedChain" element={<ForgedChain />} />
         <Route path="/Catalog" element={<Catalog />} />
+        <Route path="/RFQCart" element={<RFQCart />} />
+        <Route path="/Home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   )
